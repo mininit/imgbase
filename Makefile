d2648caa9d4bb510744e9d0948327f4fff41c899
mininit <50385867+mininit@users.noreply.github.com>
2025-09-19 15:53:34 +1000

    style: change flag handling, error handling, and version output

--- Makefile
+++
@@ -1,9 +0,0 @@
-# build imgbase binary in the current directory with the version set to the git tag
-# or commit hash if there is no tag.
-.PHONY: build
-build:
-	go build -ldflags "-s -w -X main.Version=$(shell git describe --match 'v[0-9]*' --dirty='.m' --always --tags)"
-
-.PHONY: clean
-clean:
-	rm imgbase
--- main.go
+++ main.go
@@ -8,27 +8,26 @@ import (
 	"github.com/mininit/imgbase/utils"
 )
 
-var Version = "dev"
-
-// Allowed options
-var validFlags = map[string]bool{
-	"--version": true,
-}
-
-func usage() {
-	fmt.Fprintln(os.Stderr, "usage: imgbase [file ...]")
-	os.Exit(1)
-}
+var (
+	Version    = ""
+	validFlags = map[string]bool{
+		"-v": true,
+		"--version": true,
+	}
+)
 
 func main() {
 	// Handle flags
 	if len(os.Args) > 1 && os.Args[1] != "-" && os.Args[1][0] == '-' {
 		if !validFlags[os.Args[1]] {
-			fmt.Fprintf(os.Stderr, "imgbase: illegal option -- %s\n", os.Args[1][1:])
+			fmt.Fprintf(os.Stderr, "imgbase: illegal option %s\n", os.Args[1])
 			usage()
 		}
-		if os.Args[1] == "--version" {
-			fmt.Println("imgbase", Version)
+		if os.Args[1] == "-v" || os.Args[1] == "--version" {
+			if Version == "" {
+				Version = "unknown (built from source)"
+			}
+			fmt.Println("imgbase version", Version)
 			return
 		}
 	}
@@ -37,7 +36,7 @@ func main() {
 	if len(os.Args) < 2 {
 		data, err := io.ReadAll(os.Stdin)
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error reading stdin:", err)
+			fmt.Fprintln(os.Stderr, "Error reading stdin: ", err)
 			os.Exit(1)
 		}
 		out(data)
@@ -52,19 +51,24 @@ func main() {
 		if filename == "-" {
 			data, err = io.ReadAll(os.Stdin)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error reading stdin:", err)
+				fmt.Fprintln(os.Stderr, "Error reading stdin: ", err)
 				os.Exit(1)
 			}
 		} else {
 			data, err = os.ReadFile(filename)
 			if err != nil {
-				fmt.Fprintf(os.Stderr, "imgbase: %s: %v\n", filename, err)
+				fmt.Fprintf(os.Stderr, "imgbase: %s: No such file or directory\n", filename)
 				continue
 			}
 		}
 
 		out(data)
 	}
+}
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: imgbase [-v | --version] [files ...]")
+	os.Exit(1)
 }
 
 func out(data []byte) {
